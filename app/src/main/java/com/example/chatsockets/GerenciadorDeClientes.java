beeb2f511cff99378e94add2ecabd999f8dfa4e9
imply <gabrielfontanari1@gmail.com>
2024-10-10 11:05:28 -0300

    vou começar a fazer o layout.

--- app/src/main/java/com/example/chatsockets/GerenciadorDeClientes.java
+++ app/src/main/java/com/example/chatsockets/GerenciadorDeClientes.java
@@ -1,11 +1,42 @@
 package com.example.chatsockets;
 
-import java.net.Socket;
+import android.widget.Toast;
 
-public class GerenciadorDeClientes{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.Buffer;
+
+public class GerenciadorDeClientes extends Thread{
     private Socket cliente;
+    private String nomeCliente;
 
     public GerenciadorDeClientes(Socket cliente){
         this.cliente=cliente;
+        start();
+    }
+
+    @Override
+    public void run() {
+        //ta feito com parametros para o terminal, vou modificar para o layout depois
+        try {
+            //perguntar o nome do usuario
+            BufferedReader leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            PrintWriter escritor = new PrintWriter(cliente.getOutputStream(), true);
+            String msg = leitor.readLine();
+            this.nomeCliente = msg;
+
+            while(true){
+                msg = leitor.readLine();
+                escritor.println(this.nomeCliente + "" +msg);
+            }
+
+
+        } catch (IOException e) {
+            System.err.println("O cliente fechou a conexão");
+            e.printStackTrace();
+        }
     }
 }
