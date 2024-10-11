98af124e95f4dbaf3a148c7897bded459766cd7b
EduardoT <dudu.teixeira197@gmail.com>
2024-10-11 11:02:01 -0300

    é pra estar pronto

--- app/src/main/java/com/example/chatsockets/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/chatsockets/RecyclerViewAdapter.java
@@ -8,62 +8,93 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private static final int VIEW_TYPE_SENT = 1;
-    private static final int VIEW_TYPE_RECEIVED = 2;
+    private static final int VIEW_TYPE_ENVIADA = 1;
+    private static final int VIEW_TYPE_RECEBIDA = 2;
 
-    private List<ChatMessage> chatMessages;
+    private List<ChatUsuario> listaChat;
     private String usuarioAtual; // Usuário atual
 
-    public RecyclerViewAdapter(List<ChatMessage> chatMessages, String usuarioAtual) {
-        this.chatMessages = chatMessages;
+    public RecyclerViewAdapter(List<ChatUsuario> listaUsuario, String usuarioAtual) {
+        this.listaChat = listaChat;
         this.usuarioAtual = usuarioAtual;
     }
 
     // Metodo para verificar se a mensagem é enviada ou recebida.
     @Override
     public int getItemViewType(int position) {
-        ChatMessage chatMessage = chatMessages.get(position);
+        ChatUsuario chatUsuario = listaChat.get(position);
         // Se o nome do usuário da mensagem é o mesmo do usuário atual
         // A mensagem é do tipo "enviada"
-        if (chatMessage.getUsername().equals(usuarioAtual)) {
-            return VIEW_TYPE_SENT;
+        if (chatUsuario.getUsername().equals(usuarioAtual)) {
+            return VIEW_TYPE_ENVIADA;
         } else {
-            return VIEW_TYPE_RECEIVED;
+            return VIEW_TYPE_RECEBIDA;
         }
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == VIEW_TYPE_SENT) {
+        if (viewType == VIEW_TYPE_ENVIADA) {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.chat_one_line_enviamsg, parent, false);
-            return new SentMessageViewHolder(view);
+            return new MensagemEnviadaViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.chat_one_line_recebemsg, parent, false);
+            return new MensagemRecebidaViewHolder(view);
         }
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ChatUsuario chatUsuario = listaChat.get(position);
 
+        if (holder.getItemViewType() == VIEW_TYPE_RECEBIDA) {
+            ((MensagemEnviadaViewHolder) holder).bind(chatUsuario);
+        } else {
+            ((MensagemRecebidaViewHolder) holder).bind(chatUsuario);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return chatMessages.size();
+        return listaChat.size();
     }
 
-    public static class SentMessageViewHolder extends RecyclerView.ViewHolder {
+    // ViewHolder para mensagem enviada
+    public static class MensagemEnviadaViewHolder extends RecyclerView.ViewHolder {
         TextView messageText, userNameText;
 
-        public SentMessageViewholder(@NonNull View itemView) {
+        public MensagemEnviadaViewHolder(@NonNull View itemView) {
             super(itemView);
-            messageText = itemView.findViewById((R.id.textViewUsrMsg))
+            messageText = itemView.findViewById(R.id.textViewUsrMsg);
+            userNameText = itemView.findViewById(R.id.textViewUsrName);
+        }
+
+        void bind(ChatUsuario chatUsuario) {
+            userNameText.setText("Você");
+            messageText.setText(chatUsuario.getMensagem());
+        }
+    }
+
+    // ViewHolder para mensagem recebida
+    public static class MensagemRecebidaViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText, userNameText;
+
+        public MensagemRecebidaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.textViewUsrMsg);
+            userNameText = itemView.findViewById(R.id.textViewUsrName);
+        }
+
+        void bind(ChatUsuario chatUsuario) {
+            userNameText.setText(chatUsuario.getUsername());
+            messageText.setText(chatUsuario.getMensagem());
         }
     }
 }
